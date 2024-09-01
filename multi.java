1820cc465eec4cb47a387b3ea2b68c937ea3d48a
shreyasgadave <shreyasgadave777@gmail.com>
2024-09-01 18:38:12 +0530

    add blur effect

---
+++ multi.java
@@ -0,0 +1,10 @@
+public class multi extends Thread {
+public void run(){
+    System.out.println("thread is running..");
+}
+public static void main(String[] args) {
+    multi t1=new multi();
+    t1.start();;
+}
+    
+}
